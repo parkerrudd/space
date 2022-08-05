a4e367614c1a8bdaa354e5d3495a7e04b04af037
Parker Rudd <parkerbrudd@gmail.com>
2022-08-05 13:23:27 -0600

    fixed searchNASA onChange run

--- client/src/components/NASA.js
+++ client/src/components/NASA.js
@@ -7,11 +7,18 @@ const NASA_API_BASE = 'https://images-api.nasa.gov/search?q='
 function NASA() {
 
     const [query, setQuery] = useState('')
+    const [image, setImage] = useState('')
 
-    const searchNASA = async (q) => {
-        const req = await fetch(NASA_API_BASE + q)
-        const data = await req.json()
-        console.log(data);
+    function searchNASA() {
+        const requestOptions = {
+            method: 'GET',
+            redirect: 'follow'
+        };
+          
+            fetch(NASA_API_BASE + query, requestOptions)
+            .then(response => response.json())
+            .then(result => console.log(result.collection.items[1].links[0].href))
+            .catch(error => console.log('error', error));
     }
 
 
@@ -21,8 +28,16 @@ function NASA() {
             <h1>Search the Stars</h1>
             <div className="search-nasa-api">
                 <input onChange={(e) => setQuery(e.target.value)} type="text" placeholder="Search the stars..."/>
-                <button onClick={searchNASA(query)} type="submit">Search</button>
+                <button onClick={() => searchNASA()} type="submit">Search</button>
             </div>
+
+            {image.length ? 
+                <div className="image">
+                    <img src="image" alt="NASA Space Image" />
+                </div>
+                : null
+            }
+
         </div>
     ); 
 }
